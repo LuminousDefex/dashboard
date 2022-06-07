c5359e829d710fdbbc601f8bc76073a1f00d9805
LuminousDefex <defex84@gmail.com>
2022-06-07 22:50:39 +0300

    added line chart and table

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -62,7 +62,69 @@ const Ecommerce = () => {
           ))}
         </div>
       </div>
-      <div></div>
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl-bold">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl font-semi-bold">$93,987</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor:pointer rounded-full text-white ml-3 text-xs bg-green-400">
+                    24%
+                  </span>
+                </p>
+                <p className="text-grey-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semi-bold">$48,927</span>
+                </p>
+                <p className="text-grey-500 mt-1">Expense</p>
+              </div>
+
+              <div className="mt-4">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkLine"
+                  height="80px"
+                  type="Line"
+                  data={SparklineAreaData}
+                  width="250px"
+                  color="blue"
+                />
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+            <div>
+              <Stacked width="320px" height="360px" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
